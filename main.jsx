53ee3883f9050c5007874c635dcd889c38088e32
Seventeen studio <ratsimandaimanana.thierry@gmail.com>
2025-11-20 12:15:53 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,13 +1,17 @@
+```jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 function App() {
-  return (
-    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-      <h1>TransformeSiteApp fonctionne 🎉</h1>
-      <p>Projet de test simple.</p>
-    </div>
-  );
+return (
+<div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+<h1>TransformeSiteApp fonctionne 🎉</h1>
+<p>Projet de test simple.</p>
+</div>
+);
 }
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+```
